542a685c5d365fc4f1442c3363b85b4081a3ead6
Daniel Alile <12607126+tDalile@users.noreply.github.com>
2026-01-06 15:18:49 +0100

    feat: add minimal init for local testing

---
+++ minimal_init.lua
@@ -0,0 +1,40 @@
+-- minimal_init.lua
+-- Run with: nvim -u minimal_init.lua
+
+-- 1. Setup path for lazy.nvim (we use a local .repro directory so we don't mess with your main config)
+local root = vim.fn.fnamemodify("./.repro", ":p")
+local lazypath = root .. "lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- 2. Setup lazy.nvim with this plugin locally
+require("lazy").setup({
+  {
+    "tDalile/gravel.nvim",
+    dir = ".", -- THIS IS THE KEY: Point to current directory
+    dependencies = { 
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim" -- Useful for testing :GravelDig
+    },
+    config = function()
+        require("gravel").setup({
+            path = vim.fn.expand("./gravel_dev_pit"), -- Local test pit
+            daily_format = "%Y-%m-%d",
+            follow_on_enter = true, 
+        })
+    end
+  }
+}, {
+  root = root .. "plugins", -- Install dependencies here
+  lockfile = root .. "lazy-lock.json",
+})
